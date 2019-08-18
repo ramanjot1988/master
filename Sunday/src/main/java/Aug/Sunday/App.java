435233660dcfcec3180ed268280f482323f7834e
ramanjot1988 <ramanjot.kaur.88@gmail.com>
2019-08-18 12:39:38 -0400

    new test cases added

---
+++ Sunday/src/main/java/Aug/Sunday/App.java
@@ -0,0 +1,13 @@
+package Aug.Sunday;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
